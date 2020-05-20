404f88c924768315e52cebc7bfd837446d7d77b8
Styven Florez <rflorez@condorlabs.io>
2020-05-20 14:13:14 -0500

    Feat: add Emerald-UI and node-sass

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,6 @@
  */
 
 // You can delete this file if you're not using it
+
+import 'emerald-ui/lib/styles.css'
+import './src/assets/scss/styles.scss'
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,38 +5,40 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
+import BrandLogo from '../assets/images/logo.svg'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
         }
       }
-    }
-  `)
+    `
+  )
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header siteTitle={site.siteMetadata.title} brandLogo={BrandLogo}/>
       <div
         style={{
-          margin: `0 auto`,
+          margin: '0 auto',
           maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          padding: '0 1.0875rem 1.45rem',
         }}
       >
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
-          {` `}
+          {' '}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
