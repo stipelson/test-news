8480544d8969addc76e302d1c1bb96891fefb9e0
Styven Florez <rflorez@condorlabs.io>
2020-05-21 15:48:27 -0500

    Ref: prettier code and remove footer

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
-import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
-import React from 'react'
-import Navbar from 'emerald-ui/lib/Navbar'
-import Nav from 'emerald-ui/lib/Nav'
-import DropdownItem from 'emerald-ui/lib/DropdownItem'
-import DropdownButton from 'emerald-ui/lib/DropdownButton'
-import Avatar from 'emerald-ui/lib/Avatar'
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Navbar from 'emerald-ui/lib/Navbar';
+import Nav from 'emerald-ui/lib/Nav';
+import DropdownItem from 'emerald-ui/lib/DropdownItem';
+import DropdownButton from 'emerald-ui/lib/DropdownButton';
+import Avatar from 'emerald-ui/lib/Avatar';
 // import Img from 'gatsby-image'
 
 const Header = ({ siteTitle, brandLogo }) => (
@@ -20,23 +20,21 @@ const Header = ({ siteTitle, brandLogo }) => (
         <Navbar.Brand>
           <Link to="/">
             {/* <Img fluid={''} alt={siteTitle}/> */}
-            <img src={brandLogo} alt={`Logo ${siteTitle}`}/>
+            <img src={brandLogo} alt={`Logo ${siteTitle}`} />
           </Link>
         </Navbar.Brand>
         <Nav grow collapsible>
           <DropdownButton title="Sections" id="dd1">
-            <DropdownItem eventKey="1" active>General</DropdownItem>
+            <DropdownItem eventKey="1" active>
+              General
+            </DropdownItem>
             <DropdownItem eventKey="2">Health</DropdownItem>
             <DropdownItem eventKey="3">Science</DropdownItem>
             <DropdownItem eventKey="4">Sports</DropdownItem>
             <DropdownItem eventKey="4">Technology</DropdownItem>
           </DropdownButton>
-          <Link to="/">
-            Editorial
-          </Link>
-          <Link to="/">
-            Contact us
-          </Link>
+          <Link to="/">Editorial</Link>
+          <Link to="/">Contact us</Link>
         </Nav>
         <Nav>
           <DropdownButton
@@ -57,15 +55,15 @@ const Header = ({ siteTitle, brandLogo }) => (
       </div>
     </Navbar>
   </header>
-)
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-  brandLogo: PropTypes.any
-}
+  brandLogo: PropTypes.any,
+};
 
 Header.defaultProps = {
-  siteTitle: ''
-}
+  siteTitle: '',
+};
 
-export default Header
+export default Header;
