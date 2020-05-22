67f23ebdd01c9063bfb5174733fefc935ab58e2e
Styven Florez <rflorez@condorlabs.io>
2020-05-21 22:19:40 -0500

    Fix: remove page hidden on loading

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -4,22 +4,21 @@ import Row from 'emerald-ui/lib/Row';
 import Col from 'emerald-ui/lib/Col';
 import ArticleItem from '../components/ArticleItem';
 
-const ArticlesList = ({ articles, loading }) => {
+const ArticlesList = ({ articles }) => {
   let arrayArticles = Array.isArray(articles) ? articles : [];
   return (
     <>
       <Row className="extra-p-md-6">
-        {!loading &&
-          arrayArticles.map((article, index) => (
-            <Col key={`article-${index}`} xs={6} className="mb-46">
-              <ArticleItem
-                title={article.title}
-                body={article.body}
-                image={article.image}
-                url={article.url}
-              />
-            </Col>
-          ))}
+        {arrayArticles.map((article, index) => (
+          <Col key={`article-${index}`} xs={6} className="mb-46">
+            <ArticleItem
+              title={article.title}
+              body={article.body}
+              image={article.image}
+              url={article.url}
+            />
+          </Col>
+        ))}
       </Row>
     </>
   );
